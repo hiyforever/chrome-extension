ba248ca5936717de33048431e9f81fb1bfb19bdc
hiyforever <hiyforever@users.noreply.github.com>
2020-12-28 01:42:06 +0800

    Add files via upload

--- My Extension/script_all_frames.js
+++ My Extension/script_all_frames.js
@@ -1,17 +1,37 @@
-self.addEventListener('keydown', evt => {
-    if (evt.ctrlKey) {
-        switch (evt.key) {
+self.addEventListener('keydown', e => {
+    if (e.ctrlKey) {
+        switch (e.key) {
             case 's':
             case 'd':
-                evt.preventDefault();
+                e.preventDefault();
                 break;
         }
     }
 });
-self.addEventListener('contextmenu', evt => {
+let lastSelection;
+document.addEventListener('selectionchange', () => {
+    lastSelection = document.getSelection();
+});
+self.addEventListener('mouseup', e => {
+    if (e.button != 0) {
+        return;
+    }
+    const selection = document.getSelection();
+    const text = selection.toString();
+    if (selection == lastSelection && !selection.isCollapsed && text) {
+        lastSelection = null;
+        chrome.runtime.sendMessage({
+            event: 'selectionchange',
+            data: text,
+            x: e.pageX,
+            y: e.pageY
+        });
+    }
+});
+self.addEventListener('contextmenu', e => {
     const tag = 'data-context-menu-target';
     document.querySelectorAll('[' + tag + ']').forEach(e => e.removeAttribute(tag))
-    evt.target.setAttribute(tag, true)
+    e.target.setAttribute(tag, true)
 });
 self.addEventListener('DOMSubtreeModified', e => {
     if (e.target.querySelectorAll) {
