3ca1fe33f927a757cb605eb47b824c27ba6a7fa6
hiyforever <hiyforever@users.noreply.github.com>
2021-12-28 23:34:26 +0800

    Add files via upload

--- My Extension/script_all_frames.js
+++ My Extension/script_all_frames.js
@@ -16,19 +16,44 @@ self.addEventListener('mousemove', e => {
     }
 });
 const matchPatternPage = (text, pattern) => text.trim() != '' && text.replaceAll(pattern, '').trim() == '';
-const matchPrevPage = text => matchPatternPage(text, /上一页|‹|«|<|上页|前|Previous|Prev/g);
-const matchNextPage = text => matchPatternPage(text, /下一页|›|»|>|下页|次|Next/g);
-const matchIcons = [{
-    classPrefix: 'ICN_type-',
-    code: 0xe6a6,
-}, {
-    classPrefix: 'el-icon-arrow-',
-    code: 0xe6df,
-}, {
-    classPrefix: 'fa-chevron-',
-    code: 0xf053,
-}];
-const mouseUpMatchActions = [{
+const matchPrevPage = text => matchPatternPage(text, /上一页|||<|上页|前|Previous|Prev/g);
+const matchNextPage = text => matchPatternPage(text, /下一页|||>|下页|次|Next/g);
+const isMatchPatternPage = (element, button, tagNames) => {
+    if (!tagNames.includes(element.tagName)) {
+        return false;
+    }
+    let matchPage, matchText, matchIndex;
+    switch (button) {
+        case 3: matchPage = matchPrevPage;
+            matchText = 'left';
+            matchIndex = 0;
+            break;
+        case 4: matchPage = matchNextPage;
+            matchText = 'right';
+            matchIndex = 1;
+            break;
+        default:
+            return false;
+    }
+    if (matchPage(element.textContent) || matchPage(element.title)) {
+        return true;
+    }
+    if (element.children.length == 1 && element.firstChild.tagName == 'I') {
+        for (const icon of matchIcons) {
+            if (element.firstChild.className?.includes(icon.classPrefix + matchText) &&
+                getComputedStyle(element.firstChild, ':before').content == '"' + icon.codes[matchIndex] + '"') {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+const matchIcons = [
+    { classPrefix: 'ICN_type-', codes: ['\ue6a9', '\ue6a7'] },
+    { classPrefix: 'el-icon-arrow-', codes: ['\ue6de', '\ue6e0'] },
+    { classPrefix: 'fa-chevron-', codes: ['\uf053', '\uf054'] }
+];
+const matchActions = [{
     isMatch: (element, button) => element.tagName.includes('VIDEO'),
     action: (element, button) => {
         element.currentTime += button * 10 - 35;
@@ -41,83 +66,44 @@ const mouseUpMatchActions = [{
         } + ')("' + element.tagName + '",' + button + ')';
         document.firstElementChild.appendChild(script);
         script.remove();
-    },
+    }
 }, {
-    isMatch: function (element, button) {
-        if (!['BUTTON', 'A'].includes(element.tagName)) {
-            return false;
-        }
-        let matchPage;
-        switch (button) {
-            case 3:
-                matchPage = matchPrevPage;
-                break;
-            case 4:
-                matchPage = matchNextPage;
-                break;
-            default:
-                return false;
-        }
-        return matchPage(element.textContent) || matchPage(element.title);
-    },
-    action: (element, button) => element.click(),
+    isMatch: (element, button) => isMatchPatternPage(element, button, ['BUTTON', 'A']),
+    action: (element, button) => element.click()
 }, {
-    isMatch: function (element, button) {
-        if (element.tagName != 'LI') {
-            return false;
-        }
-        let matchPage, matchText, matchOffset;
-        switch (button) {
-            case 3:
-                matchPage = matchPrevPage;
-                matchText = 'left';
-                matchOffset = 0;
-                break;
-            case 4:
-                matchPage = matchNextPage;
-                matchText = 'right';
-                matchOffset = 1;
-                break;
-            default:
-                return false;
-        }
-        if (matchPage(element.textContent) || matchPage(element.title)) {
-            return true;
-        }
-        if (element.children.length == 1 && element.firstChild.tagName == 'I') {
-            for (const icon of matchIcons) {
-                if (element.firstChild.className?.includes(icon.classPrefix + matchText) &&
-                    getComputedStyle(element.firstChild, ':before').content == unescape('"%u' + (icon.code + matchOffset).toString(16) + '"')) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    },
-    action: (element, button) => element.click(),
+    isMatch: (element, button) => isMatchPatternPage(element, button, ['LI']),
+    action: (element, button) => element.click()
 }];
 self.addEventListener('mouseup', e => {
     switch (e.button) {
         case 3:
         case 4:
             const items = Array.from(document.querySelectorAll('*')).map(element => {
-                for (const i in mouseUpMatchActions) {
-                    if (mouseUpMatchActions[i].isMatch(element, e.button)) {
+                for (const i in matchActions) {
+                    if (matchActions[i].isMatch(element, e.button)) {
                         return { id: i, element: element };
                     }
                 }
             }).filter(item => item).sort((item1, item2) => item1.id - item2.id);
-            let target = e.target;
-            while (target) {
-                for (const item of items) {
-                    if (target.contains(item.element)) {
-                        mouseUpMatchActions[item.id].action(item.element, e.button);
-                        e.preventDefault();
-                        target = undefined;
-                        break;
+            let actionItem;
+            if (items.length == 1) {
+                actionItem = items[0];
+            } else if (items.length > 1) {
+                let target = e.target;
+                while (target) {
+                    for (const item of items) {
+                        if (target.contains(item.element)) {
+                            actionItem = item;
+                            target = undefined;
+                            break;
+                        }
                     }
+                    target = target?.parentNode;
                 }
-                target = target?.parentNode;
+            }
+            if (actionItem) {
+                matchActions[actionItem.id].action(actionItem.element, e.button);
+                e.preventDefault();
             }
             break;
     }
