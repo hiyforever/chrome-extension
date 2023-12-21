77959a3818aaab80d05c8225c5789ad48b848d32
hiyforever <hiyforever@users.noreply.github.com>
2023-12-21 22:08:53 +0800

    Update script_all_frames.js

--- My Extension/script_all_frames.js
+++ My Extension/script_all_frames.js
@@ -319,7 +319,7 @@ new MutationObserver(list => {
                         e.style.backgroundColor = customColor;
                     }
                     const colors = style.color.match(/rgb\((\d+), (\d+), (\d+)\)/)?.slice(1, 4);
-                    if (colors?.every(color => color >= 128) && (e.innerText || e.value)) {
+                    if (!e.hasAttribute(colorName) && colors?.every(color => color >= 128) && (e.innerText || e.value)) {
                         let backgroundStyle = style;
                         for (let backgroundElement = e;
                             backgroundStyle?.backgroundColor == 'rgba(0, 0, 0, 0)';
