459dccf00d62e0b40071e0813c301c606e807f99
zhulin <>
2025-03-29 00:09:31 +0800

    add

--- My Extension/bilibili.js
+++ My Extension/bilibili.js
@@ -1,61 +1,65 @@
-self.addEventListener('DOMNodeInserted', e => {
-    if (e.target.classList && e.target.classList.contains('bilibili-player-ending-panel')) {
-        e.target.querySelectorAll('.bilibili-player-ending-panel-box-recommend').forEach(e => {
-            e.addEventListener('mousedown', evt => {
-                if (evt.button == 1) {
-                    evt.preventDefault();
-                }
-            });
-            e.addEventListener('mouseup', evt => {
-                if (evt.button == 1) {
-                    window.open('/video/' + e.getAttribute('data-bvid'));
-                } else if (evt.button == 0) {
-                    setTimeout(() => scrollVideoCenter(), 0);
-                }
-            });
-        });
-    } else if (e.target.classList && e.target.classList.contains('bpx-player-ending')) {
-        const items = document.querySelectorAll('.bpx-player-ending-related-item-cover');
-        const links = document.querySelectorAll('.video-page-card-small .pic a');
-        for (let i = 0; i < items.length; i++) {
-            const e = items[i];
-            e.addEventListener('mousedown', evt => {
-                if (evt.button == 1) {
-                    evt.preventDefault();
-                }
-            });
-            e.addEventListener('mouseup', evt => {
-                if (evt.button == 1) {
-                    window.open(links[i].href);
-                } else if (evt.button == 0) {
-                    setTimeout(() => scrollVideoCenter(), 0);
-                }
-            });
-        }
-    } else if (e.target.classList && (
-        e.target.classList.contains('bilibili-player-video-btn-widescreen') ||
-        e.target.classList.contains('squirtle-video-widescreen') ||
-        e.target.classList.contains('bpx-player-ctrl-wide'))) {
-        const interval = setInterval(() => {
-            if (e.target.classList.contains('closed') ||
-                e.target.classList.contains('active') ||
-                e.target.classList.contains('bpx-state-entered')) {
-                clearInterval(interval);
-                scrollVideoCenter();
-                let cnt = 2;
-                const scrollVideo = () => {
-                    if (--cnt <= 0) {
-                        document.removeEventListener('scroll', scrollVideo);
+new MutationObserver(list => {
+    list.forEach(d => d.addedNodes.forEach(e => {
+        const t = e;
+        if (t.classList && t.classList.contains('bilibili-player-ending-panel')) {
+            t.querySelectorAll('.bilibili-player-ending-panel-box-recommend').forEach(e => {
+                e.addEventListener('mousedown', evt => {
+                    if (evt.button == 1) {
+                        evt.preventDefault();
                     }
-                    scrollVideoCenter();
-                };
-                document.addEventListener('scroll', scrollVideo);
-            } else {
-                e.target.click();
+                });
+                e.addEventListener('mouseup', evt => {
+                    if (evt.button == 1) {
+                        window.open('/video/' + e.getAttribute('data-bvid'));
+                    } else if (evt.button == 0) {
+                        setTimeout(() => scrollVideoCenter(), 0);
+                    }
+                });
+            });
+        } else if (t.classList && t.classList.contains('bpx-player-ending')) {
+            const items = document.querySelectorAll('.bpx-player-ending-related-item-cover');
+            const links = document.querySelectorAll('.video-page-card-small .pic a');
+            for (let i = 0; i < items.length; i++) {
+                const e = items[i];
+                e.addEventListener('mousedown', evt => {
+                    if (evt.button == 1) {
+                        evt.preventDefault();
+                    }
+                });
+                e.addEventListener('mouseup', evt => {
+                    if (evt.button == 1) {
+                        window.open(links[i].href);
+                    } else if (evt.button == 0) {
+                        setTimeout(() => scrollVideoCenter(), 0);
+                    }
+                });
             }
-        }, 0);
-    }
-});
+        } else if (t.classList && (
+            t.classList.contains('bilibili-player-video-btn-widescreen') ||
+            t.classList.contains('squirtle-video-widescreen') ||
+            t.classList.contains('bpx-player-ctrl-wide'))) {
+            const interval = setInterval(() => {
+                if (t.classList.contains('closed') ||
+                    t.classList.contains('active') ||
+                    t.classList.contains('bpx-state-entered')) {
+                    clearInterval(interval);
+                    scrollVideoCenter();
+                    let cnt = 2;
+                    const scrollVideo = () => {
+                        if (--cnt <= 0) {
+                            document.removeEventListener('scroll', scrollVideo);
+                        }
+                        scrollVideoCenter();
+                    };
+                    document.addEventListener('scroll', scrollVideo);
+                } else {
+                    t.click();
+                }
+            }, 0);
+        }
+    }));
+}).observe(document.firstElementChild, { childList: true, subtree: true });
+
 setInterval(() => {
     if (document.querySelector('.bilibili-player-electric-panel')?.style.display == 'block') {
         document.querySelector('.bilibili-player-electric-panel-jump-content')?.click();
@@ -70,5 +74,5 @@ if (location.host == 'search.bilibili.com') {
 }
 
 function scrollVideoCenter() {
-    document.querySelector('#bilibiliPlayer, #bilibili-player').scrollIntoView({ block: 'center' });
+    document.querySelector('#bilibiliPlayer, #bilibili-player, #playerWrap').scrollIntoView({ block: 'center' });
 }
--- My Extension/bilibili_end.js
+++ My Extension/bilibili_end.js
@@ -1,8 +1,4 @@
 (() => {
-    const liveLink = document.querySelector('#i_cecream .bili-header__bar a[href~="//live.bilibili.com"]');
-    if (liveLink) {
-        liveLink.href = '//live.bilibili.com/p/eden/area-tags?areaId=0&parentAreaId=9';
-    }
     const animeLink = document.querySelector('#i_cecream .bili-header__bar a[href~="//www.bilibili.com/anime/"]');
     if (animeLink) {
         animeLink.href = '//www.bilibili.com/v/anime/serial/';
